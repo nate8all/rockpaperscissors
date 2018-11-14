e91a1584ac5ba211b0b829fcebf96be1a9c722f4
Nathan Alleyne <nathan.alleyne7@gmail.com>
2018-11-14 22:51:12 +0000

    Made adjustments to my work :)

--- js/script.js
+++ js/script.js
@@ -4,8 +4,27 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+var userChoice = "";
+var computerChoice = "";
+var winner = "";
+var randomNumber = 0;
 
 
+$("#shoot").click(function(){
+    var shoot = ("#input").val();
+    $("#userChoice").text(shoot);
+    
+    randomNumber = Math.random();
+    if (randomNumber > .4){
+        computerChoice = "rock"   
+    }
+    else if (randomNumber > .4 && randomNumber < .7){
+        computerChoice = "paper"
+    }        
+    else {
+        computerChoice = "scissors"
+    }
+});
 
 // DOCUMENT READY FUNCTION BELOW
 
